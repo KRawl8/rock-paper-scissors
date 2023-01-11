45ca1bac00357968a13f9ebf6601ef79599b6d26
Kieran <49917350+KRawl8@users.noreply.github.com>
2023-01-11 16:09:13 +0000

    Created Rock, Paper, Scissors script
    Just need to fix one bug

---
+++ script.js
@@ -0,0 +1,64 @@
+let playerScore = 0;
+let computerScore = 0;
+
+let getComputerChoice = () => {
+    let choice = Math.floor(Math.random() * 3);
+    if (choice === 0) {
+        return "Rock";
+    } else if (choice === 2) {
+        return "Paper";
+    } else {
+        return "Scissors";
+    }
+}
+
+let playRound = (playerSelection, computerSelection) => {
+    console.log(`Computers choice: ${computerSelection}`);
+    console.log(`Players choice: ${playerSelection}`);
+    if (playerSelection === computerSelection) {
+        return `You both chose ${playerSelection}. It's a draw!`;
+    } else if (playerSelection === "Rock") {
+        if (computerSelection === "Scissors") {
+            playerScore++;
+            return `You win! ${playerSelection} beats ${computerSelection}`;
+        } else {
+            computerScore++;
+            return `You lose! ${computerSelection} beats ${playerSelection}`;
+        }
+    } else if (playerSelection === "Paper") {
+        if (computerSelection === "Rock") {
+            playerScore++;
+            return `You win! ${playerSelection} beats ${computerSelection}`;
+        } else {
+            computerScore++;
+            return `You lose! ${computerSelection} beats ${playerSelection}`;
+        }
+    } else if (playerSelection === "Scissors") {
+        if (computerSelection === "Paper") {
+            playerScore++;
+            return `You win! ${playerSelection} beats ${computerSelection}`;
+        } else {
+            computerScore++;
+            return `You lose! ${computerSelection} beats ${playerSelection}`;
+        }
+    }    
+}
+
+let game = () => {
+    for (let i = 0; i < 5; i++) {
+        let playerChoice = prompt("Choose Rock, Paper or Scissors");
+        let playerSelection = playerChoice.charAt(0).toUpperCase() + playerChoice.slice(1).toLowerCase();
+        let computerSelection = getComputerChoice();
+        console.log(playRound(playerSelection, computerSelection));
+        console.log(`Player Score: ${playerScore} | Computer Score: ${computerScore}`);
+    }
+
+    if (playerScore > computerScore) {
+        console.log("You Win!");
+    } else if (playerScore === computerScore) {
+        console.log("It's a draw");
+    } else {
+        console.log("You Lose!");
+    }
+}
+console.log(game());
